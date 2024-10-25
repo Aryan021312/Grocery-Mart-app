e3e878f808b25feec86b9b26977b9cd2efbbc457
Aman <aryan981396@gmail.com>
2024-10-25 23:12:59 +0545

    Update1

--- app/src/main/java/com/example/usergrocerymart/OTPFragment.kt
+++ app/src/main/java/com/example/usergrocerymart/OTPFragment.kt
@@ -1,59 +1,137 @@
 package com.example.usergrocerymart
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import com.example.usergrocerymart.activity.UserMainActivity
+import com.example.usergrocerymart.databinding.FragmentOTPBinding
+import com.example.usergrocerymart.models.Users
+import com.example.usergrocerymart.viewmodels.authviewmodel
+import kotlinx.coroutines.launch
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [OTPFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class OTPFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private val viewModel: authviewmodel by viewModels()
+    private lateinit var binding:FragmentOTPBinding
+    private lateinit var userNumber: String
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_o_t_p, container, false)
+      binding= FragmentOTPBinding.inflate(layoutInflater)
+        getusrno()
+        edituserno()
+        sendotp()
+        loginbtnclicked()
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.backbtn.setOnClickListener {
+         findNavController().navigate(R.id.action_OTPFragment_to_signinFragment)
+        }
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment OTPFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            OTPFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+
+    private fun edituserno(){
+        val edittext= arrayOf(binding.optno1,binding.optno2,binding.optno3,binding.optno4,binding.optno5,binding.optno6)
+        for(i in edittext.indices)
+        {
+            edittext[i].addTextChangedListener(object : TextWatcher{
+                override fun beforeTextChanged(
+                    s: CharSequence?,
+                    start: Int,
+                    count: Int,
+                    after: Int
+                ) {
+
                 }
-            }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+                }
+
+                override fun afterTextChanged(s: Editable?) {
+                   if(s?.length==1)
+                   {
+                       if(i<edittext.size-1){
+                           edittext[i+1].requestFocus()
+                       }
+                   }
+                    else if(s?.length==0){
+                        if(i>0){
+                            edittext[i-1].requestFocus()
+                        }
+                   }
+                }
+
+            })
+        }
+    }
+    private fun getusrno(){
+        val bundle=arguments
+        userNumber=bundle?.getString("number").toString()
+        binding.usrphno.text=userNumber
+    }
+    private fun sendotp(){
+        mesg.showdialog(requireContext(), message = "Sending OTP...")
+        viewModel.apply { sendotp(userNumber,requireActivity())
+lifecycleScope.launch{
+    otpsent.collect{
+        if(it){
+    mesg.hidedialog()
+    mesg.showtoast(requireContext(),"OTP Sent...")
+            kotlinx.coroutines.delay(2000)}
+
     }
 }
+        }
+    }
+
+
+
+    private fun loginbtnclicked(){
+
+        binding.otploginbtn.setOnClickListener{
+            mesg.showdialog(requireContext(),"All Set...")
+            val edittext= arrayOf(binding.optno1,binding.optno2,binding.optno3,binding.optno4,binding.optno5,binding.optno6)
+            val otp=edittext.joinToString (""){it.text.toString() }
+            if(otp.length<edittext.size){
+                mesg.showtoast(requireContext(),"Invalid OTP")
+            }
+            else{
+                edittext.forEach { it.text?.clear();it.clearFocus() }
+                verifyotp(otp)
+
+            }
+        }
+    }
+
+    private fun verifyotp(otp: String) {
+val user=Users(uid = mesg.getcurrentuserid(), userPhoneNumber = userNumber, userAddress = null)
+        viewModel.signInWithPhoneAuthCredential(otp,userNumber,user)
+        lifecycleScope.launch {
+            viewModel.signindone.collect{
+                if(it){
+                    mesg.hidedialog()
+                    mesg.showtoast(requireContext(),"Welcome")
+                    startActivity(Intent(requireActivity(),UserMainActivity::class.java))
+                    requireActivity().finish()
+                }
+            }
+        }
+
+    }
+
+
+}
+
--- app/src/main/java/com/example/usergrocerymart/SigninFragment.kt
+++ app/src/main/java/com/example/usergrocerymart/SigninFragment.kt
@@ -1,59 +1,78 @@
 package com.example.usergrocerymart
 
+import android.os.Build
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
+import androidx.navigation.fragment.findNavController
+import com.example.usergrocerymart.databinding.FragmentSigninBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [SigninFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SigninFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+private lateinit var binding: FragmentSigninBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_signin, container, false)
+    ): LinearLayout {
+
+        binding=FragmentSigninBinding.inflate(layoutInflater)
+        setstatusbarcolor()
+
+        getusernumber()
+        oncontinuebuttnclick()
+        return binding.root
+    }
+    private fun oncontinuebuttnclick(){
+        binding.btncontinue.setOnClickListener{
+            val number=binding.userno.text.toString()
+            if(number.isEmpty() || number.length!=10){
+mesg.showtoast(requireContext(),"Enter Valid Phone No")
+            }
+            else{
+                val bundle=Bundle()
+                bundle.putString("number",number)
+             findNavController().navigate(R.id.action_signinFragment_to_OTPFragment,bundle)
+            }
+        }
+
+
+    }
+private fun getusernumber(){
+binding.userno.addTextChangedListener(object:TextWatcher{
+    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SigninFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SigninFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onTextChanged(number: CharSequence?, p1: Int, p2: Int, p3: Int) {
+       val len = number?.length
+        if(len == 10){
+binding.btncontinue.setBackgroundColor(ContextCompat.getColor(requireContext(),R.color.darkgreen))
+        }
+        else{
+            binding.btncontinue.setBackgroundColor(ContextCompat.getColor(requireContext(),R.color.lightgray))
+        }
+    }
+
+    override fun afterTextChanged(p0: Editable?) {
+
+    }
+
+})
+}
+
+    private fun setstatusbarcolor(){
+        activity?.window?.apply{
+            val statusbarcolor= ContextCompat.getColor(requireContext(),R.color.darkgreen)
+            statusBarColor=statusbarcolor
+            if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.M){
+                decorView.systemUiVisibility=View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
             }
+        }
     }
 }
